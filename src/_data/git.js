c1e2e6a0dd6f2f69420f618e666826a1f8959d43
ChristianBackstrom <christian.berggren.backstrom@elev.ga.ntig.se>
2021-09-14 14:28:25 +0200

    💄  removed log

--- src/_data/git.js
+++ src/_data/git.js
@@ -3,7 +3,5 @@ const axios = require('axios');
 module.exports = async() => {
     const {data} = await axios.get('https://api.github.com/users/ChristianBackstrom/repos');
 
-    console.log(data)
-
     return data;
 }
