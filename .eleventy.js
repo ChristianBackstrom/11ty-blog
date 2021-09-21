275de3aae0f952a553cebbf926e686479aa50262
ChristianBackstrom <christian.berggren.backstrom@elev.ga.ntig.se>
2021-09-21 13:01:59 +0200

    ✨ images can now be loaded

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,26 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+      outputDir: './_site/img/',
+      widths: [400, null],
+      formats: ['webp', 'avif', 'jpeg']
+  });
+
+  let imageAttributes = {
+      alt,
+      sizes,
+      loading: 'lazy',
+      decoding: 'async'
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes, {
+      whitespaceMode: 'inline'
+  });
+}
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/css');
@@ -7,6 +28,8 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPlugin(pluginRss);
+
+    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
     return {
         dir: {
---
+++ src/blog/testing-image.md
@@ -0,0 +1,11 @@
+---
+
+title: Testing Image
+
+tags: ["post"]
+
+templateEngineOverride: njk
+
+---
+
+{% image "./src/images/space.jpeg", "purple space", "(min-width: 30em) 50vw, 100vw" %}
