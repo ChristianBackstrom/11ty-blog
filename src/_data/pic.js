9372d57adc0061cfc218626846fef1f47d8f69ee
ChristianBackstrom <christian.berggren.backstrom@elev.ga.ntig.se>
2021-09-21 09:27:10 +0200

    ⚰️

--- src/_data/pic.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-module.exports = async() => {
-    const {data} = await axios.get('https://avatars.githubusercontent.com/u/54669344?v=4');
-    
-    return data;
-}
